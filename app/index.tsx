1d2cf09410b307aa19f7c673184a93c2ea218bac
Mohammed Sohail <mohammedisohail313@gmail.com>
2025-04-11 07:05:04 +0530

    project setup

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+    >
+      <Text className="text-4xl font-bold mt-8 text-center">Bismillah hir Rahmanir Rahim</Text>
+    </View>
+  );
+}
